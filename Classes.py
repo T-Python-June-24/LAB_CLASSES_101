b1f6f3e02f43ad71cf7b150640b91f5b3c02b0a0
9twy <saeed.abdullah.alghamdi@outlook.sa>
2024-06-24 14:22:13 +0300

    SOLVED BY SAEED

---
+++ Classes.py
@@ -0,0 +1,11 @@
+class Panda:
+    def __init__(self,age:int,country:str,hight:int,weight:int) -> None:
+        self.age=age
+        self.country=country
+        self.hight=hight
+        self.weight=weight
+    def getAge(self):
+        return self.age
+    def getHight(self):
+        return self.hight
+
---
+++ main.py
@@ -0,0 +1,6 @@
+import Classes 
+panda1=Classes.Panda(18,'saudia',170,200)
+panda2=Classes.Panda(25,'saudia',200,300)
+panda3=Classes.Panda(30,'sauida',150,250)
+panda4=Classes.Panda(40,'sauida',130,215)
+print(panda1.age)
